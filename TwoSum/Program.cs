6ee1911cd16c048d20b8e8a1157c11c9d1c319df
Ben Ford <ford2bm@gmail.com>
2021-10-30 11:46:58 -0600

    TwoSum

---
+++ TwoSum/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+int[] arr = {3,2,4};
+int target = 6;
+
+
+Array.ForEach(Solution.TwoSum(arr, target), Console.WriteLine);
+
+public class Solution
+{
+    public static int[] TwoSum(int[] nums, int target)
+    {
+        var len = nums.Length;
+        int[] retval = {0,1};
+        
+        if (len == 2)
+        {
+            return retval;
+        }
+
+        for (var i = 0; i < len - 1; i++)
+        {
+            for (var j = i + 1; j < len; j++)
+            {
+                if (nums[i] + nums[j] != target) continue;
+                retval[0] = i;
+                retval[1] = j;
+
+            }
+        }
+
+        return retval;
+    }
+}
+
