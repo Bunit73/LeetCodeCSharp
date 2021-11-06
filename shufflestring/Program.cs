928c5a6591e36ae7a9b861b6180de593c83e1955
Ben Ford <ford2bm@gmail.com>
2021-11-06 10:19:22 -0600

    1528. Shuffle String

---
+++ shufflestring/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+
+int[] arr = {4,5,6,7,0,2,1,3};
+string str = "codeleet";
+
+Console.WriteLine(Solution.RestoreString(str, arr));
+
+public class Solution {
+    public static string RestoreString(string s, int[] indices)
+    {
+        StringBuilder retval = new StringBuilder(s);
+        var cntr = 0;
+        foreach (var i in indices)
+        {
+            retval[i] = s[cntr];
+            cntr++;
+        }
+
+        return retval.ToString();
+    }
+}
