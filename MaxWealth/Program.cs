3eb7bad7a39ae3e3a5dc8a3b75010df959f649ea
Ben Ford <ford2bm@gmail.com>
2021-11-06 09:45:36 -0600

    1672. Richest Customer Wealth

---
+++ MaxWealth/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Solution {
+    public static int MaximumWealth(int[][] accounts)
+    {
+        var maxWealth = 0;
+        var currentWealth = 0;
+
+        foreach (var account in accounts)
+        {
+            currentWealth = 0;
+            foreach (var val in account)
+            {
+                currentWealth += val;
+            }
+
+            if (currentWealth > maxWealth)
+            {
+                maxWealth = currentWealth;
+            }
+        }
+
+        return maxWealth;
+    }
+}
