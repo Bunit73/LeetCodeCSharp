9d2ea3fb07708b83a46306687d7afecaf366fb98
Ben Ford <ford2bm@gmail.com>
2021-11-06 09:39:29 -0600

    Running Sum 1d 1480

---
+++ RunningSum1d/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] arr = {1,2,3,4};
+
+Array.ForEach(Solution.RunningSum(arr), Console.WriteLine);
+
+public class Solution
+{
+    public static int[] RunningSum(int[] nums)
+    {
+        int[] retval = new int[nums.Length];
+        var runsum = 0;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            runsum += nums[i];
+            retval[i] = runsum;
+        }
+
+        return retval;
+    }
+}
