11d087589fec2579d2ac033ed69c68861aea0760
Ben Ford <ford2bm@gmail.com>
2021-11-13 11:41:55 -0700

    739. Daily Temperatures

---
+++ DailyTemperatures/Program.cs
@@ -0,0 +1,34 @@
+﻿// https://leetcode.com/problems/daily-temperatures/
+
+int[] arr = {73,74,75,71,69,72,76,73};
+
+
+Array.ForEach(Solution.DailyTemperatures(arr), Console.WriteLine);
+
+public class Solution
+{
+    public static int[] DailyTemperatures(int[] temperatures)
+    {
+        int[] retavl = new int[temperatures.Length];
+        var daysUntilGreaterTemp = 0;
+        var currentTemp = 0;
+
+        for (var i = 0; i < temperatures.Length; i++)
+        {
+            retavl[i] = 0;
+            daysUntilGreaterTemp = 0;
+            currentTemp = temperatures[i];
+            for (var j = i + 1; j < temperatures.Length; j++)
+            {
+                daysUntilGreaterTemp++;
+                if (temperatures[j] > currentTemp)
+                {
+                    retavl[i] = daysUntilGreaterTemp;
+                    break;
+                }   
+            }
+        }
+
+        return retavl;
+    }
+}
---
+++ WaterBottles/Program.cs
@@ -0,0 +1,14 @@
+﻿// https://leetcode.com/problems/water-bottles/
+var b = 9;
+var e = 3;
+
+Console.WriteLine(Solution.NumWaterBottles(b, e));
+
+public class Solution {
+    public static int NumWaterBottles(int numBottles, int numExchange)
+    {
+        throw new NotImplementedException();
+        var retval = 0;
+        return retval;
+    }
+}
