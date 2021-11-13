24153319179c1cb3879fbc8dd33813199ee7f39b
Ben Ford <ford2bm@gmail.com>
2021-11-13 12:22:44 -0700

    9. Palindrome Number

---
+++ AddTwoNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿// https://leetcode.com/problems/add-two-numbers/
+
+ public class ListNode {
+     public int val;
+     public ListNode next;
+     public ListNode(int val=0, ListNode next=null) {
+         this.val = val;
+         this.next = next;
+     }
+ }
+
+
+public class Solution
+{
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        throw new NotImplementedException();
+    }   
+}
---
+++ PalindromeNumber/Program.cs
@@ -0,0 +1,35 @@
+﻿// https://leetcode.com/problems/palindrome-number/
+
+Console.WriteLine(Solution.IsPalindrome(1091));
+
+public class Solution
+{
+    public static bool IsPalindrome(int x)
+    {
+        var numStr = x.ToString();
+
+        if (x < 0)
+        {
+            return false;
+        }
+
+        if (numStr.Length == 1)
+        {
+            return true;
+        }
+
+        var endVal = numStr.Length -1;
+        for (var i = 0; i < numStr.Length; i++)
+        {
+            if (numStr[i] != numStr[endVal])
+            {
+                return false;
+            }
+
+            endVal--;
+        }
+
+        return true;
+
+    }
+}
