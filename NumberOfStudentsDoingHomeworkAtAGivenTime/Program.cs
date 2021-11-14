18f4fa245ac99520cf6f9065cf0c368a253fb312
Ben Ford <ford2bm@gmail.com>
2021-11-14 12:57:16 -0700

    1450. Number of Students Doing Homework at a Given Time

---
+++ NumberOfStudentsDoingHomeworkAtAGivenTime/Program.cs
@@ -0,0 +1,25 @@
+﻿// https://leetcode.com/problems/number-of-students-doing-homework-at-a-given-time/
+
+
+var start = new[] {1,2,3};
+var end = new[] {3,2,7};
+var hr = 4;
+
+Console.WriteLine(Solution.BusyStudent(start,end, hr));
+
+public class Solution {
+    public static int BusyStudent(int[] startTime, int[] endTime, int queryTime)
+    {
+        var retval = 0;
+
+        for (var i = 0; i < startTime.Length; i++)
+        {
+            if ((startTime[i] <= queryTime) && (endTime[i] >= queryTime))
+            {
+                retval++;
+            }
+        }
+
+        return retval;
+    }
+}
